b9fe384d336104b86fbfa329ad8f359bc0feec0f
jayakumar03 <jaimech61@gmail.com>
2023-09-05 13:01:08 +0530

    Added payment controllers

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,7 +1,9 @@
+const product = require("../models/product");
 const Product = require("../models/product");
 const WhereClause = require("../utils/whereClause");
 const cloudinary = require("cloudinary").v2;
 
+//& Regular user controllers
 exports.addProduct = async (req, res, next) => {
   // images
 
@@ -66,4 +68,199 @@ exports.getAllProduct = async (req, res, next) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+exports.getOneProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new Error("could not find any product form the passed id"));
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+exports.addReview = async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const alreadyReview = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (alreadyReview) {
+    product.reviews.forEach((review) => {
+      if (review.user.toString() === req.user._id.toString()) {
+        review.comment = comment;
+        review.rating = rating;
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numberOfReviews = product.reviews.length;
+  }
+
+  // Adjust ratings
+
+  product.ratings =
+    product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    product.reviews.length;
+
+  //save
+
+  await product.save({ validateBeforesave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+};
+
+exports.deleteReview = BigPromise(async (req, res, next) => {
+  const { productId } = req.query;
+
+  const product = await Product.findById(productId);
+
+  const reviews = product.reviews.filter(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  const numberOfReviews = reviews.length;
+
+  // adjust ratings
+
+  product.ratings =
+    product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    product.reviews.length;
+
+  //update the product
+
+  await Product.findByIdAndUpdate(
+    productId,
+    {
+      reviews,
+      ratings,
+      numberOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+exports.getOnlyReviewsForOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
+
+//& Admin controllers
+
+exports.adminGetAllProduct = async (req, res, next) => {
+  const products = await Product.find({});
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+exports.adminUpdateOneProduct = async (req, res, next) => {
+  try {
+    let product = await Product.findById(req.params.id);
+
+    let imageArray = [];
+
+    if (!product) {
+      return next(new Error("could not find any product form the passed id"));
+    }
+
+    if (req.files) {
+      // Deleting pre-exsiting photos
+      for (let index = 0; index < product.photos.length; index++) {
+        console.log("deleting start...");
+        await cloudinary.uploader.destroy(product.photos[index].id, {
+          folder: "products",
+        });
+      }
+
+      // Adding new photos
+
+      for (let index = 0; index < req.files.photos.length; index++) {
+        console.log("UPLOAD START...");
+        let result = await cloudinary.uploader.upload(
+          req.files.photos[index].tempFilePath,
+          {
+            folder: "products",
+          }
+        );
+
+        console.log("RESULT", result);
+        imageArray.push({
+          id: result.public_id,
+          secure_url: result.secure_url,
+        });
+      }
+
+      req.body.photos = imageArray;
+
+      // updating the new photos db
+      product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAmdModify: false,
+      });
+
+      res.status(200).json({
+        success: true,
+        product,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Files are not present in request ",
+    });
+  }
+};
+
+exports.adminDeleteOneProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new Error("could not find any product form the passed id"));
+  }
+
+  for (let index = 0; index < product.photos.length; index++) {
+    console.log("deleting start...");
+    await cloudinary.uploader.destroy(product.photos[index].id, {
+      folder: "products",
+    });
+  }
+
+  await product.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Product is removed !!",
+  });
 };
--- routes/product.js
+++ routes/product.js
@@ -6,14 +6,34 @@ const { customRole } = require("../middlewares/admin");
 const {
   addProduct,
   getAllProduct,
+  adminGetAllProduct,
+  getOneProduct,
+  adminUpdateOneProduct,
+  adminDeleteOneProduct,
+  addReview,
+  deleteReview,
+  getOnlyReviewsForOneProduct,
 } = require("../controllers/productControllers");
 
-// * userroutes
-router.route("/product").get(getAllProduct);
+//* userroutes
+router.route("/products").get(getAllProduct);
+router.route("/product/:id").get(getOneProduct);
+router.route("/review").put(isLoggedIn,addReview);
+router.route("/review").delete(isLoggedIn,deleteReview);
+router.route("/review").get(isLoggedIn,getOnlyReviewsForOneProduct);
 
-// ! Admin routes
+//! Admin routes
 router
   .route("/admin/product/add")
   .post(isLoggedIn, customRole("admin"), addProduct);
+
+router
+  .route("/adminproducts")
+  .get(isLoggedIn, customRole("admin"), adminGetAllProduct);
+
+router
+  .route("/admin/product/:id")
+  .put(isLoggedIn, customRole("admin"), adminUpdateOneProduct)
+  .delete(isLoggedIn, customRole("admin"), adminDeleteOneProduct);
 
 module.exports = router;
