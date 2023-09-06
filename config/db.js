67af9ad5ed45783cb105af1161ef7e8a9124d938
jayakumar03 <jaimech61@gmail.com>
2023-09-06 13:31:35 +0530

    updated pacakage.json

--- config/db.js
+++ config/db.js
@@ -1,15 +1,23 @@
-const mongoose = require("mongoose")
-
-
+const mongoose = require("mongoose");
 
 const connectWithDb = () => {
-    mongoose.connect(process.env.DB_URL)
+  mongoose
+    .connect(process.env.DB_URL)
     .then(() => console.log("DB connected sucessfully"))
     .catch((error) => {
-        console.log("DB connection is not sucessfully");
-        console.log(error)
-    })
-}
+      console.log("DB connection is not sucessfully");
+      console.log(error);
+    });
+};
 
+// const connectWithDb = async () => {
+//     try {
+//       const conn = await mongoose.connect(process.env.DB_URL);
+//       console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     } catch (error) {
+//       console.log(error);
+//       process.exit(1);
+//     }
+//   }
 
-module.exports = connectWithDb
+module.exports = connectWithDb;
